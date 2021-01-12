53db7a689b7ccfd16b512060d29c05cc54698ecd
KyanosTower <hdmmjohnson@gmail.com>
2021-01-11 17:24:50 -0700

    Added stylesheet and addtional js function

--- web/javascript.js
+++ web/javascript.js
@@ -1,3 +1,9 @@
-function click(){
+function clickMe(){
     alert("Clicked!");
+}
+
+function changeColor(){
+    var div = document.getElementById('div1');
+    var color = document.getElementById('textColor').value;
+    div.style.backgroundColor = color;
 }
