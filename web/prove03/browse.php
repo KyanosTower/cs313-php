f4590098e1f869417d04ae24e91ab13d0e6c2ffa
KyanosTower <hdmmjohnson@gmail.com>
2021-01-23 11:49:49 -0700

    working on session variables

--- web/prove03/browse.php
+++ web/prove03/browse.php
@@ -9,25 +9,38 @@ session_start();
 
 <body>
     <h1>All items are $15. Not a cent more, not a cent less. It's $15. Choose.</h1>
-    <form action="cart.php" method="GET">
+    <form action="" method="POST" id="ditto">
         Ditto Plush
-        <input type="checkbox" id="box1" value="Ditto Plush" />
+        <input type="checkbox" id="box1" form="DittoForm" />
+        <form action="" method="POST" id="Dice">
+            Dice
+            <input type="checkbox" name="box2" form="DiceForm" />
+        </form>
+        <form action="" method="POST" id="morrowind">
+            A used copy of The Elder Scrolls: Morrowind
+            <input type="checkbox" name="box3" form="MorrowindForm" />
+        </form>
         <br>
-        Dice
-        <input type="checkbox" name="box2" value="Dice" />
+        <a href="cart.php">Check Cart</a>
         <br>
-        A used copy of The Elder Scrolls: Morrowind
-        <input type="checkbox" name="box3" value="A used copy of The Elder Scrolls: Morrowind" />
-    </form>
-    <br>
-    <a href="cart.php">Check Cart</a>
-    <br>
-    <a href="checkout.php">Checkout</a>
-    <?php
-    if (isset($_GET['box1'])) {
-        $_SESSION['cart1'] = "Ditto Plush";
-    }
-    ?>
+        <a href="checkout.php">Checkout</a>
+        <?php
+        if (isset($_POST['box1'])) {
+            $_SESSION["cart1"] = $_POST['box1'];
+        }
+        if (isset($_POST['box2'])) {
+            $_SESSION["cart2"] = $_POST['box2'];
+        }
+        if (isset($_POST['box3'])) {
+            $_SESSION['cart3'] = $_POST['box3'];
+        }
+        /*if (isset($_POST['OrcArmor'])) {
+            $_SESSION['OrcArmor'] = $_POST['OrcArmor'];
+        }
+        if (isset($_POST['LegoGandalf'])) {
+            $_SESSION['LegoGandalf'] = $_POST['LegoGandalf'];
+        }*/
+        ?>
 </body>
 
 </html>
--- web/prove03/cart.php
+++ web/prove03/cart.php
@@ -7,23 +7,40 @@ session_start();
 <body>
     <p>You have the following items in your cart.</p>
     <?php
-    if (isset($_SESSION['$cart1'])) {
-        echo "?";
+    if (isset($_SESSION['cart1'])) {
+        echo $_SESSION["cart1"];
+        echo "<form action='' method='post' id='AndurilForm'>
+        <input type='hidden' value='Anduril' name='Anduril'>
+        <button class='btn' type='submit' form='AndurilForm' value='Anduril'>Remove from cart</button>
+        </form>";
+        if (isset($_POST['box1'])) {
+            unset($_SESSION['cart1']);
+            echo "<meta http-equiv='refresh' content='0'>"; //from https://stackoverflow.com/questions/10643626/refresh-page-after-form-submitting
+        }
     }
-    if ($_GET["box2"] == "Dice") {
-        echo $_GET["cart2"];
+    if (isset($_SESSION['cart2'])) {
+        echo $_SESSION["cart2"];
+        echo "<form action='' method='post' id='GlamdringForm'>
+        <input type='hidden' value='Glamdring' name='Glamdring'>
+        <button class='btn' type='submit' form='GlamdringForm' value='Glamdring'>Remove from cart</button>
+        </form>";
+        if (isset($_POST['box2'])) {
+            unset($_SESSION['cart2']);
+            echo "<meta http-equiv='refresh' content='0'>";
+        }
     }
-    if ($_GET["box3"] == "A used copy of The Elder Scrolls: Morrowind") {
-        echo $_GET["cart3"];
+    if (isset($_SESSION['cart3'])) {
+        echo $_SESSION['cart3'];
+        echo "<form action='' method='post' id='StingForm'>
+        <input type='hidden' value='Sting' name='Sting'>
+        <button class='btn' type='submit' form='StingForm' value='Sting'>Remove from cart</button>
+        </form>";
+        if (isset($_POST['box3'])) {
+            unset($_SESSION['cart3']);
+            echo "<meta http-equiv='refresh' content='0'>";
+        }
     }
     ?>
-    <p>Did you change your mind? Remove stuff.</p>
-    Ditto Plush
-    <input type="checkbox" name="uncheck1" value="noDitto" />
-    <br>Dice
-    <input type="checkbox" name="uncheck2" value="noDice" />
-    <br>A used copy of The Elder Scrolls: Morrowind
-    <input type="checkbox" name="uncheck3" value="noMorrowind" />
     <a href="browse.php">Return to browse</a>
 </body>
 
