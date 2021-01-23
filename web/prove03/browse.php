9bc173b669ff4e2cca4ddff5f1514d7c67baba35
KyanosTower <hdmmjohnson@gmail.com>
2021-01-23 11:24:15 -0700

    working on session variables

--- web/prove03/browse.php
+++ web/prove03/browse.php
@@ -11,7 +11,7 @@ session_start();
     <h1>All items are $15. Not a cent more, not a cent less. It's $15. Choose.</h1>
     <form action="cart.php" method="GET">
         Ditto Plush
-        <input type="checkbox" name="box1" value="Ditto Plush" />
+        <input type="checkbox" id="box1" value="Ditto Plush" />
         <br>
         Dice
         <input type="checkbox" name="box2" value="Dice" />
@@ -24,9 +24,9 @@ session_start();
     <br>
     <a href="checkout.php">Checkout</a>
     <?php
-        if (isset(['box1'])){
-            $_SESSION['cart1'] = "Ditto Plush";
-        }
+    if (isset($box1)) {
+        $_SESSION['cart1'] = "Ditto Plush";
+    }
     ?>
 </body>
 
