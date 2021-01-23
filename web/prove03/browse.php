049d5c34d67f5c98daaa25de93175ce1adfa27fe
KyanosTower <hdmmjohnson@gmail.com>
2021-01-23 10:48:31 -0700

    working on session variables

--- web/prove03/browse.php
+++ web/prove03/browse.php
@@ -2,24 +2,27 @@
 session_start();
 ?>
 <!DOCTYPE html>
+
 <head>
 
 </head>
+
 <body>
     <h1>All items are $15. Not a cent more, not a cent less. It's $15. Choose.</h1>
     <form action="cart.php" method="GET">
-    Ditto Plush
-    <input type="checkbox" name="box1" value="Ditto Plush"/>
-    <br>
-    Dice
-    <input type="checkbox" name="box2" value="Dice"/>
-    <br>
-    A used copy of The Elder Scrolls: Morrowind
-    <input type="checkbox" name="box3" value="A used copy of The Elder Scrolls: Morrowind"/>
+        Ditto Plush
+        <input type="checkbox" name="box1" value="Ditto Plush" />
+        <br>
+        Dice
+        <input type="checkbox" name="box2" value="Dice" />
+        <br>
+        A used copy of The Elder Scrolls: Morrowind
+        <input type="checkbox" name="box3" value="A used copy of The Elder Scrolls: Morrowind" />
     </form>
     <br>
     <a href="cart.php">Check Cart</a>
     <br>
     <a href="checkout.php">Checkout</a>
 </body>
+
 </html>
