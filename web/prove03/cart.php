5edd422b78112c717c8262afc59e6b50f118f8dc
KyanosTower <hdmmjohnson@gmail.com>
2021-01-23 12:15:37 -0700

    working on session variables

--- web/prove03/cart.php
+++ web/prove03/cart.php
@@ -1,49 +1,52 @@
 <?php
-if (!isset($_SESSION)) {
-    session_start();
+if (!isset($_SESSION)) { session_start(); }
+
+echo '<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Checkout</title>
+</head>
+<body>';
+
+echo 'Your Cart Contains:<br><br>';
+if(isset($_SESSION['Ditto'])){
+  echo $_SESSION["Ditto"];
+  echo "<form action='' method='post' id='DittoForm'>
+  <input type='hidden' value='Ditto' name='Ditto'>
+  <button class='btn' type='submit' form='DittoForm' value='Ditto'>Remove from cart</button>
+  </form>";
+  if(isset($_POST['Ditto'])){
+    unset($_SESSION['Ditto']);
+    echo "<meta http-equiv='refresh' content='0'>";
 }
-?>
+}
+if(isset($_SESSION['Dice'])){
+  echo $_SESSION["Dice"];
+  echo "<form action='' method='post' id='DiceForm'>
+  <input type='hidden' value='Dice' name='Dice'>
+  <button class='btn' type='submit' form='DiceForm' value='Dice'>Remove from cart</button>
+  </form>";
+  if(isset($_POST['Dice'])){
+    unset($_SESSION['Dice']);
+    echo "<meta http-equiv='refresh' content='0'>";
+  }
+}
+if(isset($_SESSION['Morrowind'])){
+  echo $_SESSION['Morrowind'];
+  echo "<form action='' method='post' id='MorrowindForm'>
+  <input type='hidden' value='Sting' name='Morrowind'>
+  <button class='btn' type='submit' form='MorrowindForm' value='Morrowind'>Remove from cart</button>
+  </form>";
+  if(isset($_POST['Morrowind'])){
+    unset($_SESSION['Morrowind']);
+    echo "<meta http-equiv='refresh' content='0'>";
+  }
+}
 
-<!DOCTYPE html>
+echo '<br>' . '<a href="index.php"><- Back to store</a>';
+echo '<br><br>' . '<a href="checkout.php">Checkout -></a>';
 
-<body>
-    <p>You have the following items in your cart.</p>
-    <?php
-    if (isset($_SESSION['cart1'])) {
-        echo $_SESSION["cart1"];
-        echo "<form action='' method='post' id='AndurilForm'>
-        <input type='hidden' value='Anduril' name='Anduril'>
-        <button class='btn' type='submit' form='AndurilForm' value='Anduril'>Remove from cart</button>
-        </form>";
-        if (isset($_POST['box1'])) {
-            unset($_SESSION['cart1']);
-            echo "<meta http-equiv='refresh' content='0'>"; //from https://stackoverflow.com/questions/10643626/refresh-page-after-form-submitting
-        }
-    }
-    if (isset($_SESSION['cart2'])) {
-        echo $_SESSION["cart2"];
-        echo "<form action='' method='post' id='GlamdringForm'>
-        <input type='hidden' value='Glamdring' name='Glamdring'>
-        <button class='btn' type='submit' form='GlamdringForm' value='Glamdring'>Remove from cart</button>
-        </form>";
-        if (isset($_POST['box2'])) {
-            unset($_SESSION['cart2']);
-            echo "<meta http-equiv='refresh' content='0'>";
-        }
-    }
-    if (isset($_SESSION['cart3'])) {
-        echo $_SESSION['cart3'];
-        echo "<form action='' method='post' id='StingForm'>
-        <input type='hidden' value='Sting' name='Sting'>
-        <button class='btn' type='submit' form='StingForm' value='Sting'>Remove from cart</button>
-        </form>";
-        if (isset($_POST['box3'])) {
-            unset($_SESSION['cart3']);
-            echo "<meta http-equiv='refresh' content='0'>";
-        }
-    }
-    ?>
-    <a href="browse.php">Return to browse</a>
-</body>
-
-</html>
+echo '</body></html>';
