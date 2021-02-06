8d4e7b8ac89b492b3f7552dd71d4447008643785
KyanosTower <hdmmjohnson@gmail.com>
2021-02-06 13:16:49 -0700

    data added, now working on UI

--- web/prove05/geographic.php
+++ web/prove05/geographic.php
@@ -3,14 +3,19 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Homepage</title>
+    <title>Geographic Page</title>
 </head>
 
 <body style="text-align: center;">
-    <h1>Pick a table to look at please.</h1>
+    <h1>You have two options, pick one.</h1>
     <a href="https://dry-depths-02343.herokuapp.com/prove05/character.php">Character</a>
     <a href="https://dry-depths-02343.herokuapp.com/prove05/geographic.php">Geographic</a>
     
+    <footer>
+        <p>
+            <a href="https://dry-depths-02343.herokuapp.com/prove05/home.php">Click here to return to the Homepage.</a>
+        </p>
+    </footer>
 </body>
 
 </html>
