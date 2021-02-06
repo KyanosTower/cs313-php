de6143e0080b76a4f6d284362e34d2fe512f77af
KyanosTower <hdmmjohnson@gmail.com>
2021-02-06 12:34:53 -0700

    added base php

---
+++ web/prove05/home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Homepage</title>
+</head>
+
+<body>
+    <h1>Hey look at some test data</h1>
+    <?php
+    try {
+        $dbUrl = getenv('DATABASE_URL');
+
+        $dbOpts = parse_url($dbUrl);
+
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"], '/');
+
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $ex) {
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+    }
+    foreach ($db->query('SELECT name, class FROM character') as $row) {
+        echo 'Name: ' . $row['name'];
+        echo 'Class: ' . $row['class'];
+        echo '<br/>';
+    }
+    ?>
+</body>
+
+</html>
