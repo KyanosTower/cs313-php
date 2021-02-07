d0cd349bde2078f17e4cf3fd5e6b3982084fd3c6
KyanosTower <hdmmjohnson@gmail.com>
2021-02-06 21:27:39 -0700

    adding user input

--- web/prove05/geographic.php
+++ web/prove05/geographic.php
@@ -31,7 +31,7 @@ try {
 <body style="text-align: center;">
     <h1>Input a name.</h1>
     <form id="getInput" action="" method="GET">
-        <input type="text" name="nameInput" id="nameInput" value="Enter Name" />
+        <input type="text" name="nameInput" id="nameInput" value='<?php echo $name; ?>' />
     </form>
     <button name="notHidden" onclick="displayView()">Search</button>
     <form id="displayData" style="visibility:hidden">
